05b4231eb325018b05968daf22e24e3bb86fc5f5
aubustou <survivalfr@yahoo.fr>
2021-04-19 13:29:31 +0200

    Add multidimensional keywords with different rates

--- parse_cv.py
+++ parse_cv.py
@@ -6,15 +6,20 @@ from pathlib import Path
 from pdfminer.high_level import extract_text
 
 LOCAL_FOLDER = Path()
-KEYWORDS = []
+LANGUAGE_KEYWORDS: list[str] = []
+LANGUAGE_KEYWORDS_RATE = 10
+HOBBY_KEYWORDS: list[str] = []
+HOBBY_KEYWORDS_RATE = 2
 KEPT_CVS_PATH = Path()
 
 
 def parse_cv(file_path: Path) -> bool:
     content = extract_text(file_path)
-    logging.info([x.lower() in content.lower() for x in KEYWORDS])
 
-    cv_rate = sum(x.lower() in content.lower() for x in KEYWORDS)
+    cv_rate = sum(x.lower() in content.lower() for x in
+                  LANGUAGE_KEYWORDS) * LANGUAGE_KEYWORDS_RATE
+    cv_rate += sum(
+        x.lower() in content.lower() for x in HOBBY_KEYWORDS) * HOBBY_KEYWORDS_RATE
 
     if cv_rate:
         logging.info("Found CV %s rated %s", str(file_path), cv_rate)
@@ -22,23 +27,28 @@ def parse_cv(file_path: Path) -> bool:
         rate_folder.mkdir(exist_ok=True)
         shutil.copy(file_path,
                     KEPT_CVS_PATH / ("rated_" + str(cv_rate)) / file_path.name)
+        return True
     else:
         logging.debug("CV %s does not contain keywords", str(file_path))
+        return False
 
 
 def main():
-    global LOCAL_FOLDER, KEYWORDS, KEPT_CVS_PATH
+    global LOCAL_FOLDER, LANGUAGE_KEYWORDS, HOBBY_KEYWORDS, KEPT_CVS_PATH
 
     parser = argparse.ArgumentParser(
         description="Sort CVs according to keywords. Rate them too")
     parser.add_argument("--folder", type=Path, default=Path(),
                         help="folder where CVs are located")
-    parser.add_argument("--keywords", nargs="+",
+    parser.add_argument("--language-keywords", nargs="+",
+                        help="keywords to find")
+    parser.add_argument("--hobby-keywords", nargs="+",
                         help="keywords to find")
 
     args = parser.parse_args()
     LOCAL_FOLDER = args.folder
-    KEYWORDS = args.keywords
+    LANGUAGE_KEYWORDS = args.language_keywords
+    HOBBY_KEYWORDS = args.hobby_keywords
 
     logging.basicConfig(level=logging.INFO)
     logging.getLogger("pdfminer").setLevel(logging.WARNING)
