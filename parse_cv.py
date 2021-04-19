cc86fcdd81d1aaad8a7795ccd49cea028034aec2
aubustou <survivalfr@yahoo.fr>
2021-04-19 13:49:14 +0200

    Add possibility to use a file of links

--- parse_cv.py
+++ parse_cv.py
@@ -5,15 +5,13 @@ from pathlib import Path
 
 from pdfminer.high_level import extract_text
 
-LOCAL_FOLDER = Path()
 LANGUAGE_KEYWORDS: list[str] = []
 LANGUAGE_KEYWORDS_RATE = 10
 HOBBY_KEYWORDS: list[str] = []
 HOBBY_KEYWORDS_RATE = 2
-KEPT_CVS_PATH = Path()
 
 
-def parse_cv(file_path: Path) -> bool:
+def parse_cv(file_path: Path, destination_folder: Path) -> bool:
     content = extract_text(file_path)
 
     cv_rate = sum(x.lower() in content.lower() for x in
@@ -23,30 +21,51 @@ def parse_cv(file_path: Path) -> bool:
 
     if cv_rate:
         logging.info("Found CV %s rated %s", str(file_path), cv_rate)
-        rate_folder = KEPT_CVS_PATH / ("rated_" + str(cv_rate))
+        rate_folder = destination_folder / ("rated_" + str(cv_rate))
         rate_folder.mkdir(exist_ok=True)
         shutil.copy(file_path,
-                    KEPT_CVS_PATH / ("rated_" + str(cv_rate)) / file_path.name)
+                    destination_folder / ("rated_" + str(cv_rate)) / file_path.name)
         return True
     else:
         logging.debug("CV %s does not contain keywords", str(file_path))
         return False
 
 
+from typing import Iterable
+
+
+def iter_on_files(local_folder: Path, path_file: Path) -> Iterable[Path]:
+    if path_file:
+        yield Path(path_file.read_text())
+    else:
+        yield from local_folder.iterdir()
+
+
 def main():
-    global LOCAL_FOLDER, LANGUAGE_KEYWORDS, HOBBY_KEYWORDS, KEPT_CVS_PATH
+    global LANGUAGE_KEYWORDS, HOBBY_KEYWORDS
 
     parser = argparse.ArgumentParser(
         description="Sort CVs according to keywords. Rate them too")
-    parser.add_argument("--folder", type=Path, default=Path(),
-                        help="folder where CVs are located")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--folder", type=Path,
+                       help="folder where CVs are located")
+    group.add_argument("--file", type=Path,
+                       help="file with links to the files or websites")
+    parser.add_argument("--destination-folder", type=Path,
+                        help="folder where the CVs will be copied")
     parser.add_argument("--language-keywords", nargs="+",
                         help="keywords to find")
     parser.add_argument("--hobby-keywords", nargs="+",
                         help="keywords to find")
 
     args = parser.parse_args()
-    LOCAL_FOLDER = args.folder
+    if not args.folder or args.file:
+        parser.print_usage()
+
+    local_folder = args.folder if args.folder else Path()
+    destination_folder = args.destination_folder if args.destination_folder else local_folder / "kept"
+    destination_folder.mkdir(exist_ok=True)
+
     LANGUAGE_KEYWORDS = args.language_keywords
     HOBBY_KEYWORDS = args.hobby_keywords
 
@@ -54,15 +73,12 @@ def main():
     logging.getLogger("pdfminer").setLevel(logging.WARNING)
     logging.info("Start")
 
-    KEPT_CVS_PATH = LOCAL_FOLDER / "kept"
-    KEPT_CVS_PATH.mkdir(exist_ok=True)
-
-    for file_ in LOCAL_FOLDER.iterdir():
+    for file_ in iter_on_files(local_folder, args.file):
         logging.debug(str(file_))
         if file_.suffix not in {".pdf"}:
             continue
 
-        parse_cv(file_)
+        parse_cv(file_, destination_folder)
 
 
 if __name__ == '__main__':
